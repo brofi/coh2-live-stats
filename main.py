440145b5bcf6d7a26d2ebfff6be23b9534166fc3
andy <brofi.archlinux@gmail.com>
2024-02-14 11:30:31 +0100

    Format player ranks output

--- main.py
+++ main.py
@@ -31,6 +31,8 @@ class Player(NamedTuple):
     relic_id: int
     team: int
     faction: Faction
+    rank: int
+    rank_level: int
 
 
 logfile = Path.home().joinpath('Documents', 'My Games', 'Company of Heroes 2', 'warnings.log')
@@ -43,43 +45,54 @@ for line in lines:
     if player_line:
         player_lines.append(player_line.strip())
 
-last_match_player_count = int(player_lines[len(player_lines) - 1].split(' ')[0]) + 1
-player_lines = player_lines[len(player_lines) - last_match_player_count:]
+# TODO watch file
+if not player_lines:
+    print('No player data found.')
+    exit(0)
+
+latest_match_player_count = int(player_lines[len(player_lines) - 1].split(' ')[0]) + 1
+# 1v1: 0, 2v2: 1, 3v3: 2, 4v4: 3
+game_mode = (latest_match_player_count / 2) - 1
+# Keep only players of latest match
+player_lines = player_lines[len(player_lines) - latest_match_player_count:]
 
 players = []
+request_url = "https://coh2-api.reliclink.com/community/leaderboard/GetPersonalStat?title=coh2&profile_ids=[{}]"
+
+leaderboard_id = -1
 for player_line in player_lines:
+    # Get player data from log file
     s = player_line.split(' ')
     faction = faction_from_log(s.pop())
     team = int(s.pop())
     relic_id = int(s.pop())
     player_id = int(s.pop(0))
     name = ' '.join(s)
-    players.append(Player(player_id, name, relic_id, team, faction))
+    rank = -1
+    rank_level = -1
 
-api = "https://coh2-api.reliclink.com/community/leaderboard/GetPersonalStat?title=coh2&profile_ids="
-
-# TODO make single request for all players
-# request_url = api + '['
-# for player in players:
-#     request_url += str(player.relic_id) + ','
-# request_url += ']'
-# print(request_url)
-
-# 1v1: 0, 2v2: 1, 3v3: 2, 4v4: 3
-game_mode = (last_match_player_count / 2) - 1
-
-for player in players:
-    leaderboard_id = -1
-    if player.faction.id == 4:
+    # Request player data from CoH2 API
+    if faction.id == 4:
         leaderboard_id = 51 + game_mode
     else:
         # leaderboard_id 0..3 -> AI Games
-        leaderboard_id = 4 + (game_mode * 4) + player.faction.id
+        leaderboard_id = 4 + (game_mode * 4) + faction.id
 
-    r_url = api + '[' + str(player.relic_id) + ']'
-    r = requests.get(r_url)
-
+    r = requests.get(request_url.format(relic_id))
     for d in r.json()['leaderboardStats']:
         if d['leaderboard_id'] == leaderboard_id:
-            print('#' + str(d['rank']) + ' (' + str(d['ranklevel']) + ') ' + player.name)
+            rank = d['rank']
+            rank_level = d['ranklevel']
             break
+
+    players.append(Player(player_id, name, relic_id, team, faction, rank, rank_level))
+
+row = '| {} | {} | {}'
+sep = ('+ ' + '-' * 5 + ' + ' + '-' * 3 + ' + ' + '-' * 32) * 2 + ' +'
+print(sep)
+print(row.format('Rank'.ljust(5), 'Lvl', 'Name'.ljust(32)) * 2 + ' |')
+print(sep)
+for player in players:
+    s = row.format(str(player.rank).rjust(5), str(player.rank_level).rjust(3), player.name.ljust(32))
+    print(s, end='') if player.team == 0 else print(s + ' |')
+print(sep)

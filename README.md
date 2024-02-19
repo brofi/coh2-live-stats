cf2df145d1f539dc990352093b9bb420fe1cc799
andy <brofi.archlinux@gmail.com>
2024-02-19 05:18:50 +0100

    PyInstaller config

--- README.md
+++ README.md
@@ -1,46 +1,85 @@
 # CoH2LiveStats
 
-
 ## Setup project
 
 * Create Virtual Environment with IDE or with:
+
 ```console
 $ python -m venv venv
 ```
+
 * Restart IDE or activate venv e.g. for PowerShell with:
+
 ```console
 $ .\venv\Scripts\activate.ps1
 ```
+
 * Install dependencies
 
-## Build
+## Make package
 
-Install `build`:
+### setuptools & build
+
+* Install `build`:
 
 ```console
 $ pip install build
 ```
 
-Run `build`:
+* Run `build`:
 
 ```console
 $ python -m build
 ```
 
-## Install
+* Install package:
 
 ```console
 $ pip install .\dist\coh2_live_stats-0.0.1-py3-none-any.whl
 ```
 
-## Run
+* Run installed module:
 
 ```console
 $ python -m coh2_live_stats
 ```
 
-or
+* Run installed script:
 
 ```console
 $ coh2livestats.exe
 ```
+
+### PyInstaller
+
+* Install `pyinstaller`:
+
+```console
+$ pip install pyinstaller
+```
+
+* Run `scripts/pyinstaller_run.py` or:
+
+```console
+$ pyinstaller ^
+--noconfirm ^
+--name coh2livestats ^
+--contents-directory lib ^
+--add-data COPYING:. ^
+--icon .\res\coh2_live_stats.ico ^
+.\src\coh2_live_stats\__main__.py
+```
+
+* Run `pyinstaller` with generated `.spec` file:
+
+```console
+$ pyinstaller CoH2LiveStats.spec
+```
+
+* Run generated script:
+
+```console
+$ .\dist\CoH2LiveStats\CoH2LiveStats.exe
+```
+
+* Distribute archive of `.\dist\CoH2LiveStats`
---
+++ scripts/pyinstaller_run.py
@@ -0,0 +1,39 @@
+#  Copyright (C) 2024 Andreas Becker.
+#
+#  This file is part of CoH2LiveStats.
+#
+#  CoH2LiveStats is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+#  License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+#  later version.
+#
+#  Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+#  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along with Foobar. If not,
+#  see <https://www.gnu.org/licenses/>.
+import os
+from pathlib import Path
+
+import PyInstaller.__main__
+
+license_file_name = 'COPYING'
+app_name = 'CoH2LiveStats'
+contents_dir_name = 'lib'
+content_root = Path().cwd().parent
+dist_path = content_root.joinpath('dist')
+
+PyInstaller.__main__.run([
+    '--distpath', str(dist_path),
+    '--workpath', str(content_root.joinpath('build')),
+    '--noconfirm',
+    '--specpath', str(content_root),
+    '--name', app_name,
+    '--contents-directory', contents_dir_name,
+    '--add-data', '{}:.'.format(license_file_name),
+    '--icon', str(content_root.joinpath('res', 'coh2_live_stats.ico')),
+    str(content_root.joinpath('src', 'coh2_live_stats', '__main__.py'))
+])
+
+# Move license next to executable
+app_path = dist_path.joinpath(app_name)
+os.replace(app_path.joinpath(contents_dir_name, license_file_name), app_path.joinpath(license_file_name))

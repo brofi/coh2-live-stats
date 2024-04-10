brofi/coh2-live-stats

commit 621b22bf82eec198fb08e9c61fff73a34e7fed74

Fix: output no longer cleared for new matches

src/coh2_live_stats/output.py
@@ -14,6 +14,7 @@
 #  CoH2LiveStats. If not, see <https://www.gnu.org/licenses/>.
 
 import logging
+import os
 from functools import partial
 from typing import Any
 
@@ -23,6 +24,7 @@ from prettytable.colortable import ColorTable, Theme
 from .data.countries import country_set
 from .data.faction import Faction
 from .data.match import Match
+from .data.player import Player
 from .settings import Settings
 from .util import cls_name
 
@@ -84,7 +86,14 @@ class Output:
     def _get_column_index(self, col):
         return self.table.field_names.index(col.label)
 
-    def print_match(self, match: Match):
+    def print_match(self, players: list[Player]):
+        self.clear()
+
+        if not players:
+            print('Waiting for match...')
+            return
+
+        match = Match(players)
         cols = self.settings.table.columns
         for party_index, party in enumerate(match.parties):
             for player_index, player in enumerate(party.players):
@@ -221,6 +230,14 @@ class Output:
                 print(self.table)
         else:
             LOG.warning('No table columns to print.')
+
+    def clear(self):
+        if os.name == 'nt':
+            _ = os.system('cls')
+            print('\b', end='')
+        else:
+            _ = os.system('clear')
+        self.table.clear_rows()
 
     def _format_faction(self, _, v):
         colored = self.settings.table.color

src/coh2_live_stats/__main__.py
@@ -37,7 +37,6 @@ from watchdog.observers import Observer
 # Since the creation of a virtual environment this somehow works without the project
 # being installed.
 from coh2_live_stats.coh2api import CoH2API
-from coh2_live_stats.data.match import Match
 from coh2_live_stats.data.player import Player
 from coh2_live_stats.logging_conf import (
     HiddenOutputFilter,
@@ -47,7 +46,6 @@ from coh2_live_stats.logging_conf import (
 from coh2_live_stats.output import Output
 from coh2_live_stats.settings import Settings, SettingsFactory
 from coh2_live_stats.util import (
-    clear,
     cls_name,
     cls_name_parent,
     is_running_in_pyinstaller,
@@ -156,7 +154,7 @@ class LogFileEventHandler(FileSystemEventHandler):
 def on_players_gathered(future_players):
     if new_match_found:
         with suppress(concurrent.futures.CancelledError):
-            print_match(future_players.result())
+            output.print_match(future_players.result())
 
 
 async def init_leaderboards():
@@ -180,14 +178,6 @@ async def get_players(*, notify=True):
     return None
 
 
-def print_match(players: list[Player]):
-    if not players:
-        print('Waiting for match...')
-    else:
-        clear()
-        output.print_match(Match(players))
-
-
 async def main():
     global api, settings, output, EXIT_STATUS
 
@@ -202,7 +192,7 @@ async def main():
         output = Output(settings)
         # Initial requests
         await init_leaderboards()
-        print_match(await get_players(notify=False))
+        output.print_match(await get_players(notify=False))
         # Watch log files
         handler = LogFileEventHandler(asyncio.get_running_loop())
         logfile_dir = str(settings.logfile.parent)

src/coh2_live_stats/util.py
@@ -14,7 +14,6 @@
 #  CoH2LiveStats. If not, see <https://www.gnu.org/licenses/>.
 
 import asyncio
-import os
 import sys
 import winsound
 from inspect import isclass
@@ -30,14 +29,6 @@ def cls_name_parent(obj: type | object) -> str | None:
 
 def ratio(x, total) -> float:
     return x / total if total > 0 else 0
-
-
-def clear():
-    if os.name == 'nt':
-        _ = os.system('cls')
-        print('\b', end='')
-    else:
-        _ = os.system('clear')
 
 
 async def progress_start():

commit 2781f4a10509d2be3eee74cedadf60dee7248262

Fix bug introduced in 621b22bf: missing column

src/coh2_live_stats/output.py
@@ -36,12 +36,13 @@ LOG = logging.getLogger('coh2_live_stats')
 class Output:
     def __init__(self, settings: Settings):
         self.settings = settings
-        self.table = self._create_output_table(settings)
-        LOG.info('Initialized %s[columns=%s]', cls_name(self), self.table.field_names)
+        self.table = self._create_output_table()
+        self._set_formatters()
+        LOG.info('Initialized %s', cls_name(self))
 
-    def _create_output_table(self, settings) -> PrettyTable:
-        if settings.table.color:
-            border_color = str(settings.table.colors.border.value)
+    def _create_output_table(self) -> PrettyTable:
+        if self.settings.table.color:
+            border_color = str(self.settings.table.colors.border.value)
             table = ColorTable(
                 theme=Theme(
                     vertical_color=border_color,
@@ -52,10 +53,13 @@ class Output:
         else:
             table = PrettyTable()
 
-        table.border = settings.table.border
+        table.border = self.settings.table.border
         table.preserve_internal_border = True
 
-        cols = settings.table.columns
+        return table
+
+    def _set_field_names(self):
+        cols = self.settings.table.columns
         visible_columns = [
             (c.label, c.align)
             for c in sorted(
@@ -66,20 +70,22 @@ class Output:
                 key=lambda c: c.pos,
             )
         ]
-        table.field_names = [label for label, _ in visible_columns]
+        self.table.field_names = [label for label, _ in visible_columns]
 
         for label, align in visible_columns:
-            table.align[label] = align
+            self.table.align[label] = align
 
-        table.custom_format[cols.faction.label] = self._format_faction
-        table.custom_format[cols.rank.label] = partial(self._format_rank, 2)
-        table.custom_format[cols.level.label] = partial(self._format_rank, 1)
+        LOG.info('Output columns: %s', self.table.field_names)
+
+    def _set_formatters(self):
+        cols = self.settings.table.columns
+        self.table.custom_format[cols.faction.label] = self._format_faction
+        self.table.custom_format[cols.rank.label] = partial(self._format_rank, 2)
+        self.table.custom_format[cols.level.label] = partial(self._format_rank, 1)
         for c in cols.win_ratio, cols.drop_ratio:
-            table.custom_format[c.label] = self._format_ratio
+            self.table.custom_format[c.label] = self._format_ratio
         for c in cols.prestige, cols.country, cols.name:
-            table.custom_format[c.label] = self._format_min_max
-
-        return table
+            self.table.custom_format[c.label] = self._format_min_max
 
     def _set_column(self, row, col, val):
         if col.visible:
@@ -89,13 +95,14 @@ class Output:
         return self.table.field_names.index(col.label)
 
     def print_match(self, players: list[Player]):
-        self.clear()
+        self._clear()
 
         if not players:
             print('Waiting for match...')
             return
 
         match = Match(players)
+        self._set_field_names()
         cols = self.settings.table.columns
         for party_index, party in enumerate(match.parties):
             for player_index, player in enumerate(party.players):
@@ -233,13 +240,13 @@ class Output:
         else:
             LOG.warning('No table columns to print.')
 
-    def clear(self):
+    def _clear(self):
         if os.name == 'nt':
             _ = os.system('cls')
             print('\b', end='')
         else:
             _ = os.system('clear')
-        self.table.clear_rows()
+        self.table.clear()
 
     @staticmethod
     async def progress_start():
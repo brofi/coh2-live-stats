brofi/coh2-live-stats

commit 5f4db3ddd5b4308260488e205f0f38220c42d2b3

Show 0% ratios

src/coh2_live_stats/output.py
@@ -300,8 +300,8 @@ class Output:
             v_str = f'{v[0]}{v[1]}'
         return self._format_min_max(_, (v_str, v[2], v[3]))
 
-    def _format_ratio(self, f: str, v: float | Empty) -> str:
-        if not v:
+    def _format_ratio(self, f: str, v: float | None) -> str:
+        if v is None:
             return ''
 
         colored = self.settings.table.color

src/coh2_live_stats/data/player.py
@@ -84,12 +84,12 @@ class Player:
         return self.wins + self.losses
 
     @property
-    def win_ratio(self) -> float:
+    def win_ratio(self) -> float | None:
         """Player's wins relative to the number of played games."""
         return ratio(self.wins, self.num_games)
 
     @property
-    def drop_ratio(self) -> float:
+    def drop_ratio(self) -> float | None:
         """Player's drops relative to the number of played games."""
         return ratio(self.drops, self.num_games)
 

src/coh2_live_stats/util.py
@@ -39,9 +39,12 @@ def cls_name_parent(obj: type | object) -> str | None:
     return obj.mro()[1].__name__ if isclass(obj) else obj.__class__.mro()[1].__name__
 
 
-def ratio(x: float, total: float) -> float:
+def ratio(x: float, total: float) -> float | None:
     """X relative to total."""
-    return x / total if total > 0 else 0
+    try:
+        return x / total
+    except ZeroDivisionError:
+        return None
 
 
 def stop_sound() -> None:

commit 763e47bc8239b8e7071f6d951ee4e63aadef73e9

Fix: crash when processing avg row (introduced in 5f4db3dd)

src/coh2_live_stats/output.py
@@ -300,8 +300,8 @@ class Output:
             v_str = f'{v[0]}{v[1]}'
         return self._format_min_max(_, (v_str, v[2], v[3]))
 
-    def _format_ratio(self, precision: int, f: str, v: float | None) -> str:
-        if v is None:
+    def _format_ratio(self, precision: int, f: str, v: float | Empty) -> str:
+        if not isinstance(v, float):
             return ''
 
         colored = self.settings.table.color